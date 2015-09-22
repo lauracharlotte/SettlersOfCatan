037de270a906b1d244aee7c969dc313ee7043300
madisonjbrooks12 <madisonjbrooks12@gmail.com>
2015-09-22 17:24:31 -0600

    Added ClientModelSupplier

---
+++ java/src/model/ClientModelSupplier.java
@@ -0,0 +1,54 @@
+package model;
+
+public class ClientModelSupplier {
+	
+	/**
+	 * Static instance of the ClientModelSupplier
+	 */
+	private static ClientModelSupplier _instance = null;
+	
+	/**
+	 * Current version of the ClientModel
+	 */
+	private ClientModel currentModel;
+	
+	/**
+	 * Gets the singleton instance of the ClientModelSupplier
+	 * @return the ClientModelSupplier
+	 */
+	public static ClientModelSupplier getInstance()
+	{
+		if (_instance == null)
+		{
+			_instance = new ClientModelSupplier();
+		}
+		return _instance;
+	}
+	
+	/**
+	 * Private constructor for the ClientModelSupplier; only called by getInstance()
+	 */
+	private ClientModelSupplier()
+	{
+		currentModel = null;
+	}
+	
+	/**
+	 * Sets the current ClientModel
+	 * @param newModel the ClientModel to set
+	 */
+	public void setModel(ClientModel newModel)
+	{
+		currentModel = newModel;
+	}
+	
+	/**
+	 * Gets the current ClientModel
+	 * @return the ClientModel
+	 */
+	public ClientModel getModel()
+	{
+		return currentModel;
+	}
+
+}
