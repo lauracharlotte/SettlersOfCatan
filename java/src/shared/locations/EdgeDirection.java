5690d4e84d414074f08faa87b232fc53d5c50450
mwalker9 <walker.mwalker9@gmail.com>
2015-10-17 16:41:41 -0600

    Copied edge direction to vertex direction
    copied convertEdgeDirToVertexDir into EdgeDirection class for use in map
    controller.

--- java/src/shared/locations/EdgeDirection.java
+++ java/src/shared/locations/EdgeDirection.java
@@ -21,5 +21,39 @@ public enum EdgeDirection
 	{
 		return opposite;
 	}
+        
+        public VertexDirection[] convertEdgeDirToVertexDir()
+        {
+            VertexDirection[] vertexDirections = new VertexDirection[2];
+            switch(this)
+            {
+                case North:
+                    vertexDirections[0] = VertexDirection.NorthEast;
+                    vertexDirections[1] = VertexDirection.NorthWest;
+                    break;
+                case NorthWest:
+                    vertexDirections[0] = VertexDirection.West;
+                    vertexDirections[1] = VertexDirection.NorthWest;
+                    break;
+                case NorthEast:
+                    vertexDirections[0] = VertexDirection.NorthEast;
+                    vertexDirections[1] = VertexDirection.East;
+                    break;
+                case South:
+                    vertexDirections[0] = VertexDirection.SouthEast;
+                    vertexDirections[1] = VertexDirection.SouthWest;
+                    break;
+                case SouthWest:
+                    vertexDirections[0] = VertexDirection.West;
+                    vertexDirections[1] = VertexDirection.SouthWest;
+                    break;
+                case SouthEast:
+                    vertexDirections[0] = VertexDirection.SouthEast;
+                    vertexDirections[1] = VertexDirection.East;
+                    break;
+            }
+            return vertexDirections;
+        }
+        
 }
 
