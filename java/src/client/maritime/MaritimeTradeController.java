0c797905ef31524ef9aa8fb9f09d3a133ebea8e3
mwalker9 <walker.mwalker9@gmail.com>
2015-10-26 19:00:09 -0600

    Maritime looks at cities now too
    Cities on ports now count

--- java/src/client/maritime/MaritimeTradeController.java
+++ java/src/client/maritime/MaritimeTradeController.java
@@ -152,11 +152,11 @@ public class MaritimeTradeController extends Controller implements IMaritimeTrad
         CatanMap map = ClientModelSupplier.getInstance().getModel().getMap();
         ArrayList<Port> ports = (ArrayList<Port>) map.getPorts();
         ArrayList<VertexObject> settlements = (ArrayList<VertexObject>) map.getSettlements();
+        settlements.addAll(map.getCities());
         ResourceCards playerCards = ClientModelSupplier.getInstance().getClientPlayerObject().getHand().getResourceCards();
         resources = getFourTrade(resources, playerCards);
-        for (int i = 0; i < settlements.size(); i++)
+        for (VertexObject settlement : settlements)
         {
-            VertexObject settlement = settlements.get(i);
             if (settlement.getOwner().getIndex() == playerIdx.getIndex())
             {
                 for (int j = 0; j < ports.size(); j++)
