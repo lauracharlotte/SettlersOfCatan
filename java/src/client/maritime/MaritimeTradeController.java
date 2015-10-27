cf90100ecf43e2d0fa87a1fe965b12c2fe80d7e9
mwalker9 <walker.mwalker9@gmail.com>
2015-10-26 19:05:20 -0600

    Fixed enabling of all items after unset
    Maritime allowed you to select any resource if you undid the "give"
    resource.  Fixed it.

--- java/src/client/maritime/MaritimeTradeController.java
+++ java/src/client/maritime/MaritimeTradeController.java
@@ -126,8 +126,9 @@ public class MaritimeTradeController extends Controller implements IMaritimeTrad
         ratio = 0;
         this.tradeOverlay.setTradeEnabled(false);
         this.tradeOverlay.hideGetOptions();
-        this.tradeOverlay.showGiveOptions(enabledResources);
         this.tradeOverlay.reset();
+        this.tradeOverlay.showGiveOptions(enabledResources);
+        
     }
 
     @Override
