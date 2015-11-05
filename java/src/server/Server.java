8e9172bbc86a6f4308f1024967ab5ab4fc911ea8
mwalker9 <walker.mwalker9@gmail.com>
2015-11-04 21:29:57 -0700

    Put something in the Server class
    Added the Main function to the server class

---
+++ java/src/server/Server.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server;
+
+/**
+ *
+ * @author Michael
+ */
+public class Server
+{
+
+    /**
+     * Starts a Java Server that can handle httpRequests dealing with Settlers of Catan.
+     * @param args args[0] must be the port number the server should be ran on
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+}
