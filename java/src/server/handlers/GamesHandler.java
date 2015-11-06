ba20882490921d5a90fc8dee787ecdfdf0fcfa7c
madisonjbrooks12 <madisonjbrooks12@gmail.com>
2015-11-05 18:31:54 -0700

    Games handler implements HttpHandler

--- java/src/server/handlers/GamesHandler.java
+++ java/src/server/handlers/GamesHandler.java
@@ -1,9 +1,25 @@
 package server.handlers;
 
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
 /**
  * Handler for games requests
  */
-public class GamesHandler 
+public class GamesHandler implements HttpHandler
 {
-
+	private CookieVerifier verifier;
+	
+	/**
+    *
+    * @param he The current httpExchange -- should only correspond to a join, create, or list request
+    * @throws IOException When the connection is bad in some way
+    */
+   @Override
+   public void handle(HttpExchange he) throws IOException
+   {
+       throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   }
 }
