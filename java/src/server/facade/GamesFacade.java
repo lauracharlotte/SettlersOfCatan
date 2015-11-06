150df93c4869664b0d6b145d75b8661549a892de
madisonjbrooks12 <madisonjbrooks12@gmail.com>
2015-11-05 18:14:10 -0700

    Worked on javadocs

--- java/src/server/facade/GamesFacade.java
+++ java/src/server/facade/GamesFacade.java
@@ -1,11 +1,59 @@
 package server.facade;
 
+import java.util.List;
+
+import model.ClientModel;
+import model.player.User;
+
 /**
- * 
+ * Facade to the server model that deals with games operations
  * @author Madison Brooks
  *
  */
 public class GamesFacade implements IGamesFacade 
 {
+	private List<User> users;
+	private List<ClientModel> games;
+	
+	/**
+	 * Constructor
+	 * @param users Non-persistent list of Users
+	 * @param games Non-persistent list of games
+	 */
+	public GamesFacade(List<User> users, List<ClientModel> games)
+	{
+		this.users = users;
+		this.games = games;
+	}
+	
+	/**
+	 * Gets the list of games
+	 */
+	@Override
+	public void list() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * Creates a new game
+	 */
+	@Override
+	public void create() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public void join() 
+	{
+		// TODO Auto-generated method stub
+		
+	}
 
 }
