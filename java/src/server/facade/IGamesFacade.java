99b848994bc2dbd7c7c3d0c716cc053a7a009832
madisonjbrooks12 <madisonjbrooks12@gmail.com>
2015-11-05 18:58:10 -0700

    Added cookie verifier, extended Server from HttpServer, fixed games facades

--- java/src/server/facade/IGamesFacade.java
+++ java/src/server/facade/IGamesFacade.java
@@ -1,5 +1,10 @@
 package server.facade;
 
+import java.util.List;
+
+import model.ClientModel;
+import model.player.User;
+
 /**
  * Interface for facades that deal with games operations
  * @author Madison Brooks
@@ -7,7 +12,22 @@ package server.facade;
  */
 public interface IGamesFacade extends IModelFacade 
 {
-	public void list();
-	public void create();
-	public void join();
+	/**
+	 * Gets the list of games
+	 * @return list of games in the form of ClientModels
+	 */
+	public List<ClientModel> list();
+	
+	/**
+	 * Creates a new game
+	 */
+	public ClientModel create();
+	
+	/**
+	 * Joins a user to a game
+	 * @param user the User to be joined
+	 * @param gameIndex the index of the game in the list of ClientModels
+	 */
+	public ClientModel join(User user, int gameIndex);
+
 }
---
+++ java/src/server/handlers/CookieVerifier.java
@@ -0,0 +1,21 @@
+package server.handlers;
+
+import clientcommunicator.Server.Cookie;
+
+/**
+ * Checks validity of cookies coming from the client
+ * @author Madison Brooks
+ *
+ */
+public class CookieVerifier 
+{
+	/**
+	 * Verifies a cookie
+	 * @param cookie the cookie to be checked
+	 * @return true if valid, false otherwise
+	 */
+	public boolean isVerified(Cookie cookie)
+	{
+		return false;
+	}
+}
