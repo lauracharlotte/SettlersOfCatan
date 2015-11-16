2034adf2a849d7c8e73a8c258d0d73de8f0ddbef
mwalker9 <walker.mwalker9@gmail.com>
2015-11-16 11:21:25 -0700

    Game and Moves Handler
    Wrote code to make game and moves handler run

--- java/src/server/handlers/MovesHandler.java
+++ java/src/server/handlers/MovesHandler.java
@@ -8,6 +8,10 @@ package server.handlers;
 import clientcommunicator.Server.Cookie;
 import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import server.ServerException;
+import server.command.ICommand;
 import server.facade.IModelFacade;
 
 /**
@@ -24,12 +28,34 @@ public class MovesHandler extends AbstractHandler
     @Override
     public void reallyHandle(HttpExchange he, Cookie currentCookie) throws IOException
     {
-        if (currentCookie.getCompleteCookieString().equals(""))
+        if (currentCookie.getGameNumber() < 0 || currentCookie.getUser() == null)
         {
             this.sendQuickResponse(he, "Missing necessary cookie.", 400);
             return;
         }
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ICommand command;
+        try
+        {
+            command = this.getCommand(he);
+        }
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex)
+        {
+            Logger.getLogger(MovesHandler.class.getName()).log(Level.SEVERE, null, ex);
+            this.sendQuickResponse(he, "Something died", 400);
+            return;
+        }
+        String result;
+        try
+        {
+            result = command.execute(currentFacade, this.getRequestBody(he), currentCookie);
+        }
+        catch (ServerException ex)
+        {
+            Logger.getLogger(MovesHandler.class.getName()).log(Level.SEVERE, null, ex);
+            this.sendQuickResponse(he, "Something died", 400);
+            return;
+        }
+        this.sendQuickResponse(he, result, 200);
     }
     
 }
