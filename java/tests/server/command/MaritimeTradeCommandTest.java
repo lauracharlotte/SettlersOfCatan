e839a8e55b891e242727c9cbb8b4c3fa5bc7c5d3
mwalker9 <walker.mwalker9@gmail.com>
2015-11-17 22:06:42 -0700

    Created stubs for command JUnit Tests
    Stubs for JUnit tests

---
+++ java/tests/server/command/MaritimeTradeCommandTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server.command;
+
+import clientcommunicator.Server.Cookie;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import server.facade.IModelFacade;
+
+/**
+ *
+ * @author Michael
+ */
+public class MaritimeTradeCommandTest
+{
+    
+    public MaritimeTradeCommandTest()
+    {
+    }
+    
+    @BeforeClass
+    public static void setUpClass()
+    {
+    }
+    
+    @AfterClass
+    public static void tearDownClass()
+    {
+    }
+    
+    @Before
+    public void setUp()
+    {
+    }
+    
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void testExecute() throws Exception
+    {
+        System.out.println("execute");
+        IModelFacade facade = null;
+        String requestBody = "";
+        Cookie currentCookie = null;
+        MaritimeTradeCommand instance = new MaritimeTradeCommand();
+        String expResult = "";
+        String result = instance.execute(facade, requestBody, currentCookie);
+        assertEquals(expResult, result);
+        fail("The test case is a prototype.");
+    }
+    
+}
