ea7da1df72e4f91c9d7229f5d63ca4af17d7e70e
mwalker9 <walker.mwalker9@gmail.com>
2015-11-18 15:59:46 -0700

    Send Chat Test
    Tests send chat

--- java/tests/server/command/SendChatCommandTest.java
+++ java/tests/server/command/SendChatCommandTest.java
@@ -6,6 +6,17 @@
 package server.command;
 
 import clientcommunicator.Server.Cookie;
+import clientcommunicator.operations.SendChatRequest;
+import java.util.ArrayList;
+import java.util.Collection;
+import model.ClientModel;
+import model.cards.DevelopmentCards;
+import model.cards.Hand;
+import model.cards.ResourceCards;
+import model.player.Player;
+import model.player.PlayerIdx;
+import model.player.TurnStatusEnumeration;
+import model.player.User;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -13,6 +24,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import server.facade.IModelFacade;
+import server.facade.IMovesFacade;
+import server.facade.MovesFacade;
+import server.model.GameManager;
+import shared.definitions.CatanColor;
+import shared.locations.HexLocation;
 
 /**
  *
@@ -48,15 +64,41 @@ public class SendChatCommandTest
     @Test
     public void testExecute() throws Exception
     {
-        System.out.println("execute");
-        IModelFacade facade = null;
-        String requestBody = "";
-        Cookie currentCookie = null;
-        SendChatCommand instance = new SendChatCommand();
-        String expResult = "";
-        String result = instance.execute(facade, requestBody, currentCookie);
-        assertEquals(expResult, result);
-        fail("The test case is a prototype.");
+        System.out.println("execute send chat");
+        GameManager manager= new GameManager();
+        manager.addNewGame(new ClientModel(true, false, true, "name"));
+        IMovesFacade facade = new MovesFacade(manager);
+        ClientModel currentModel = manager.getGameWithNumber(0);
+        Collection<Player> players = new ArrayList<>();
+        DevelopmentCards emptyDevCards = new DevelopmentCards(1, 1, 1, 1, 1);
+        ResourceCards ResCards = new ResourceCards(3, 3, 3, 3, 3);
+        Player newPlayer = new Player(1, CatanColor.PUCE, false, 0, "bobby", null, false, 
+                        emptyDevCards, 0, 11, 3, 0, 0, new Hand(ResCards, emptyDevCards));
+        newPlayer.setSoldiers(2);
+        players.add(newPlayer);
+        newPlayer.setPlayerIndex(new PlayerIdx(0));
+        ResCards = new ResourceCards(3, 3, 3, 3, 3);
+        newPlayer = new Player(1, CatanColor.PURPLE, false, 0, "bobby2", null, false, 
+                        emptyDevCards, 1, 11, 3, 0, 0, new Hand(ResCards, emptyDevCards));
+        players.add(newPlayer);
+        newPlayer.setPlayerIndex(new PlayerIdx(1));
+        currentModel.setPlayers(players);
+        if(currentModel.getMap().getRobber().equals(new HexLocation(0,0)))
+            currentModel.getMap().setRobber(new HexLocation(1,0));
+        currentModel.getTurnTracker().setStatus(TurnStatusEnumeration.playing);
+        manager.replaceGame(0, currentModel);
+        SendChatRequest req = new SendChatRequest(new PlayerIdx(0), "request");
+        String request = req.serialize();
+        ICommand sendChatCmd = new SendChatCommand();
+        Cookie currentCookie = new Cookie();
+        currentCookie.setGameNumber(0);
+        User newUser = new User("bobby", "bobby");
+        newUser.setPlayerId(0);
+        currentCookie.setUser(newUser);
+        sendChatCmd.execute(facade, request, currentCookie);
+        assert(1 == manager.getGameWithNumber(0).getChat().getLines().size());
+        sendChatCmd.execute(facade, request, currentCookie);
+        assert(manager.getGameWithNumber(0).getChat().getLines().size() == 2);
     }
     
 }
