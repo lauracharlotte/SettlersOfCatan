a1826efdd7d7335d4ad6c9fc4643e0f97b2160ce
mwalker9 <walker.mwalker9@gmail.com>
2015-12-01 18:28:44 -0700

    Different Types of Persistence

---
+++ java/src/server/persistence/db/DBGameAccess.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server.persistence.db;
+
+import server.IGameAccess;
+
+/**
+ *
+ * @author Michael
+ */
+public class DBGameAccess implements IGameAccess
+{
+    
+}
---
+++ java/src/server/persistence/file/FileGameAccess.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package server.persistence.file;
+
+import server.IGameAccess;
+
+/**
+ *
+ * @author Michael
+ */
+public class FileGameAccess implements IGameAccess
+{
+    
+}
