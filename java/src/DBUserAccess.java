f78916236ce0c30b4536328f7cb7f7b7798208fd
Scott Hampton <scott.l.hampton@gmail.com>
2015-12-08 19:55:18 -0700

    Made a jar file for the db factory

--- java/src/DBUserAccess.java
+++ java/src/DBUserAccess.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package server.persistence.db;
+//package server.persistence.db;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
